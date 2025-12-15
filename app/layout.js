28b5feb07e3edd95f700fbd5eabc71121f0b7b01
smallbizsolutions <austinrnorthrop@gmail.com>
2025-12-14 22:39:32 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Outfit } from 'next/font/google'
 import SessionGuard from '@/components/SessionGuard'
 import CookieConsent from '@/components/CookieConsent'
+import AmexBackground from '@/components/AmexBackground'
 
 const outfit = Outfit({ subsets: ['latin'] })
 
@@ -14,9 +15,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={outfit.className}>
-        <SessionGuard />
-        {children}
-        <CookieConsent />
+        {/* Background sits at z-index: 0 */}
+        <AmexBackground />
+
+        {/* ✅ App content sits above at z-index: 10 */}
+        <div className="relative z-10 min-h-screen w-full">
+          <SessionGuard />
+          {children}
+          <CookieConsent />
+        </div>
       </body>
     </html>
   )
