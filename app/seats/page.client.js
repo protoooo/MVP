69cc16b1243cf64a487a476a6513f919927a099c
smallbizsolutions <austinrnorthrop@gmail.com>
2025-12-28 09:12:46 -0500

    Redirect to homepage if no session is found

--- app/seats/page.client.js
+++ app/seats/page.client.js
@@ -95,7 +95,7 @@ export default function SeatsClient() {
     try {
       const { data } = await supabase.auth.getSession()
       if (!data.session) {
-        window.location.href = '/auth'
+        window.location.href = '/'
         return
       }
 
