bf3455ead1204c9058d4fda3397957820df8c2c8
protocolLM0RG <support@protocollm.org>
2025-12-31 08:17:00 -0500

    Update __init__.py

--- inspection_scraper/scrapers/__init__.py
+++ inspection_scraper/scrapers/__init__.py
@@ -6,31 +6,4 @@ UPDATED: Now includes Sword Solutions JavaScript-based scraper
 from .base_scraper import BaseScraper
 from .sword_solutions import SwordSolutionsScraper
 
-# Keep legacy scrapers for backward compatibility
-try:
-    from .washtenaw import WashtenawScraper
-except ImportError:
-    WashtenawScraper = None
-
-try:
-    from .wayne import WayneScraper
-except ImportError:
-    WayneScraper = None
-
-try:
-    from .oakland import OaklandScraper
-except ImportError:
-    OaklandScraper = None
-
-__all__ = [
-    'BaseScraper',
-    'SwordSolutionsScraper',
-]
-
-# Add legacy scrapers if they exist
-if WashtenawScraper:
-    __all__.append('WashtenawScraper')
-if WayneScraper:
-    __all__.append('WayneScraper')
-if OaklandScraper:
-    __all__.append('OaklandScraper')
+__all__ = ['BaseScraper', 'SwordSolutionsScraper']
